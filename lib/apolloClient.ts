8d8443e152addb163747d419c5fc739204e8e60a
Silke Goris <r0674618@student.thomasmore.be>
2026-03-06 21:40:45 +0100

    please work

--- lib/apolloClient.ts
+++ lib/apolloClient.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
 export const client = new ApolloClient({
   link: new HttpLink({
-    uri: "http://localhost:3000/api/graphql",
+    uri: "/api/graphql",
   }),
   cache: new InMemoryCache(),
 });
